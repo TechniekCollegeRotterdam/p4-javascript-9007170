65be73cee2994f938a0ae285dec53e2afc702378
9007170 <9007170@student.zadkine.nl>
2022-05-17 12:37:29 +0200

    17-5-2022

--- les 1/app.js
+++ les 1/app.js
@@ -3,8 +3,21 @@
 let name;
 let country;
 let p = document.getElementById("output");
+let nameChange = document.getElementById("nameChange");
+let countryChange = document.getElementById("countryChange")
 
-const nameChanged = function(event){
+
+nameChange.addEventListener ("change", function(){
+    name = event.target.value;
+    changeInput();
+})
+
+countryChange.addEventListener("change",function(){
+    country = event.target.value;
+    changeInput();
+})
+
+/* const nameChanged = function(event){
     // het event loggen in het console 
     name = event.target.value;
     changeInput();
@@ -19,5 +32,8 @@ const countryChange = function(event){
 const changeInput = function(){
     const text = `Je heet ${name} en je komt uit ${country}`;     
     p.textContent = text;
-}
+} */
+
+
+
  
